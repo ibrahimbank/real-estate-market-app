7c4afa45cb5a17df167aebdd84dbb8ba4e3c5379
bankole <bankoleibrahim96@gmail.com>
2022-01-25 20:03:40 +0100

    Designed the sign in and sign up page

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -6,15 +6,84 @@ import visibilityIcon from "../assets/svg/visibilityIcon.svg";
 function SignUp() {
   const [showPassword, setShowPassword] = useState(false);
   const [formData, setFormData] = useState({
+    name: "",
     email: "",
     password: "",
   });
 
-  const { email, password } = formData;
+  const { name, email, password } = formData;
+
+  const navigate = useNavigate();
+
+  const onChange = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.id]: e.target.value,
+    }));
+  };
 
   return (
     <>
-      <h1> Sign Up</h1>
+      <div className="pageContainer">
+        <header>
+          <p className="pageHeader">Welcome Back!</p>
+        </header>
+
+        <main>
+          <form>
+            <input
+              type="text"
+              className="nameInput"
+              placeholder="Name"
+              id="name"
+              value={name}
+              onChange={onChange}
+            />
+            <input
+              type="email"
+              className="emailInput"
+              placeholder="Email"
+              id="email"
+              value={email}
+              onChange={onChange}
+            />
+            <div className="passwordInputDiv">
+              <input
+                type={showPassword ? "text" : "password"}
+                className="passwordInput"
+                placeholder="password"
+                id="password"
+                value={password}
+                onChange={onChange}
+              />
+
+              <img
+                src={visibilityIcon}
+                alt="show password"
+                className="showPassword"
+                onClick={() => setShowPassword((prevState) => !prevState)}
+              />
+            </div>
+
+            <Link to="/forgot-password" className="forgotPasswordLink">
+              Forgot Password
+            </Link>
+
+            <div className="signUpBar">
+              <p className="signUpText">Sign Up</p>
+              <button className="signUpButton">
+                <ArrowRightIcon fill="#ffffff" width="34px" height="34px" />
+              </button>
+            </div>
+          </form>
+
+          {/* Google aut */}
+
+          <Link to="/signin" className="registerLink">
+            Sign In Instead
+          </Link>
+        </main>
+      </div>
     </>
   );
 }
